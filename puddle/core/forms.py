f0cf1c6bc53fa19de0dc72d3d4b1124e7f2f50ff
Rudy Cabrera <rudycabrera.ma@gmail.com>
2023-04-21 02:56:17 +0000

    new changes

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -12,8 +12,21 @@ class SignupForm(UserCreationForm):
         username = forms.CharField(widget=forms.TextInput(attrs={
             'placeholder': 'Your username',
             'class':'w-full py-4 px-6 rounded-xl',
-        }
+        }))
 
+        email = forms.CharField(widget=forms.EmailInput(attrs={
+            'placeholder': 'Your email address',
+            'class':'w-full py-4 px-6 rounded-xl',
+        }))
 
-))
+        password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password',
+            'class':'w-full py-4 px-6 rounded-xl',
+        }))
+
+        password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat password',
+            'class':'w-full py-4 px-6 rounded-xl',
+        }))
+
 
--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item ##This will allow us to display the products
 
@@ -26,11 +26,21 @@ def contact(request):
 
 
 def signup(request):
-    form = SignupForm()
+    if request.method =='POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+
+            return redirect('/login/')
+        
+        else:
+            form = SignupForm()
 
     return render(request, 'core/signup.html',{
 
-        'form':form
+            'form':form
 
     }
     
