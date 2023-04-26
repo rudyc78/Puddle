b2b5d7fbcfb1b1080be9f9c5469cccb66e5768a5
Rudy Cabrera <rudycabrera.ma@gmail.com>
2023-04-26 03:04:53 +0000

    new updates

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -7,9 +7,6 @@ from .forms import SignupForm
 
 
 
-
-
-
 def index(request):
     items = Item.objects.filter(is_sold =False)[0:6]##This will allow us to see six items
     categories = Category.objects.all()
@@ -17,32 +14,26 @@ def index(request):
     return render(request, 'core/index.html', {
         'categories': categories,
         'items': items,
-    }
-    
-    )
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
 
 
-def signup(request):
-    if request.method =='POST':
+def signup(request):       
+    if request.method == 'POST':
         form = SignupForm(request.POST)
+       
 
         if form.is_valid():
             form.save()
 
 
             return redirect('/login/')
-        
-        else:
-            form = SignupForm()
+    else:
+        form = SignupForm()
 
     return render(request, 'core/signup.html',{
+        'form':form
 
-            'form':form
-
-    }
-    
-    
-    )
+    })
